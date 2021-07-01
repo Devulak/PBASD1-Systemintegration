f2b4fb45b356214bdf87c8558793d87b036d62ea
Devulak <3112566+ZeroXitreo@users.noreply.github.com>
2021-07-01 17:05:09 +0200

    Add filter module

---
+++ Exam/FilterBoi/FilterBoi/Program.cs
@@ -0,0 +1,58 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Buffers.Binary;
+
+namespace FilterBoi
+{
+    class Program
+    {
+        private static int numToAdd;
+
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                Console.WriteLine("What channel are we consuming on?");
+                string consumeChannel = Console.ReadLine();
+                Console.WriteLine("What channel are we publishing on?");
+                string publishChannel = Console.ReadLine();
+
+                Random rnd = new();
+                numToAdd = rnd.Next(0, 50);
+                Console.WriteLine("Rolling the dice... " + numToAdd + "!");
+
+                channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+                // Input channel
+                channel.QueueDeclare(consumeChannel, true, false, false);
+
+                EventingBasicConsumer consumer = new(channel);
+                consumer.Received += (sender, ea) =>
+                {
+                    int bodyAsNumber = BinaryPrimitives.ReadInt32LittleEndian(ea.Body.Span);
+                    Console.WriteLine(" [x] Received {0}", bodyAsNumber);
+                    Console.WriteLine("Adding {0} to number... {1}, {2}!", numToAdd, bodyAsNumber, bodyAsNumber += numToAdd);
+                    Console.WriteLine(" [x] Publishing to {0}", publishChannel);
+
+                    byte[] body = BitConverter.GetBytes(bodyAsNumber);
+
+                    channel.BasicPublish(exchange: ea.Exchange,
+                                         routingKey: publishChannel,
+                                         basicProperties: ea.BasicProperties,
+                                         body: body);
+
+                    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                };
+                channel.BasicConsume(consumeChannel, false, consumer);
+
+                Console.WriteLine(" [*] Waiting for messages.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Exam/FilterBoi/WorkAssignment/Program.cs
@@ -0,0 +1,63 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                string queue;
+                do
+                {
+                    Console.WriteLine("What channel are we publishing on?");
+                    queue = Console.ReadLine();
+                } while (string.IsNullOrEmpty(queue));
+
+                var arguments = new Dictionary<string, object>
+                {
+                    {"x-dead-letter-exchange","DeadLetterExchange" }
+                };
+
+                channel.QueueDeclare(queue: queue,
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: arguments);
+
+                while (true)
+                {
+                    int number;
+                    bool? gotNumber = null;
+                    do
+                    {
+                        if (gotNumber != null)
+                        {
+                            Console.WriteLine("That's not a number!");
+                        }
+                        Console.WriteLine("What number should we start with?");
+                        string numberAsString = Console.ReadLine();
+                        gotNumber = int.TryParse(numberAsString, out number);
+                    } while (!gotNumber.HasValue || gotNumber != true);
+
+                    byte[] body = BitConverter.GetBytes(number);
+
+                    IBasicProperties properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: queue,
+                                         basicProperties: properties,
+                                         body: body);
+                    Console.WriteLine(" [x] Sent to channel {0} with body: {1}", queue, number);
+                }
+            }
+        }
+    }
+}
